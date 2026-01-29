1f6b799a3ebfeaf8cc370c8384bb987fc8335b87
gaurav <manubhaikindarkhediya1@gmail.com>
2026-01-29 12:07:18 +0530

    [gk][new added��]

--- obj.js
+++ obj.js
@@ -19,3 +19,22 @@ console.log("person");
 
 delete person.salary;
 console.log(person);
+
+const person1={
+    name:"dip",
+    age:30,
+    addres:{
+        city:'ksd',
+        zipcode:'362220',
+        street:'123 main street'
+    }
+}
+
+ console.log(person1);
+ console.log(person1?.addres?.city);
+ console.log(Object.keys(person1));
+ console.log(Object.values(person1));
+ console.log(Object.entries(person1));       
+
+
+ 
