e57756a8a69b1122a92e355b9c5195edbcfb04b0
gaurav <manubhaikindarkhediya1@gmail.com>
2026-01-29 08:48:19 +0530

    [gk][done]

---
+++ methods.js
@@ -0,0 +1,32 @@
+// string methods
+// length()
+// trim ()
+// Ltrim()left side space remove in text 
+// Rtrim() right side space remove in text
+// toUpperCase()
+// toLowerCase()
+// includes()
+// indexOf()
+// slice()
+// substring()
+// replace()
+// contact ()
+// split()
+
+let str = "Hello javascript word";
+console.log(str);
+
+console.log("length():", str.length);
+
+let trimText = str.trim();
+console.log("trim():", trimText);
+
+console.log("toUpperCase():", str.toUpperCase());
+
+console.log("toLowerCase():", str.toLowerCase());
+
+console.log("includes():", str.includes("javascript"));
+
+console.log("indexOf():", str.indexOf("javascript"));
+
+console.log("slice():", str.slice(6, 16));
