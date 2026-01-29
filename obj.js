9c474c6e72a40febdb51931083352189a72007f4
gaurav <manubhaikindarkhediya1@gmail.com>
2026-01-29 11:55:15 +0530

    [PU][message] git commit -m[gk][message]

--- obj.js
+++ obj.js
@@ -1,1 +1,21 @@
 // object is collection of any data 
+const person = {
+    name: "gaurav",
+    age: 30,
+   hobbies: ["cricket", "listen music" ],
+    greet : function() {
+        console.log("Hello");
+    }
+}
+
+// access values from object
+console.log( person);
+console.log(person['age']);
+
+// add or modify the data of object 
+person .job="react developer";
+person.salary=50000;
+console.log("person");
+
+delete person.salary;
+console.log(person);
